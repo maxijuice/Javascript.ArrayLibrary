47d7aa98543e56abd3156093d87bf7eb23f940c1
Maksim Bulakhau <maksim.bulakhau@gmail.com>
2017-03-28 13:41:11 +0300

    ArrayLibrary done. Refactored

--- test/arrayLib.test.js
+++ test/arrayLib.test.js
@@ -4,66 +4,66 @@ Unit tests for arrayLibrary via jasmine
 describe("ArrayLibrary tests", function() {
 
     beforeEach(function(){
-        test_array = [1,2,3,4,5,6,7,8];
+        testArray = [1,2,3,4,5,6,7,8];
     });
 
     describe("Method take tests", function() {
         it("returns {1,2} on ({1,2,3,4}, 2)", function(){
-            expect(arrayLibrary.take(test_array,2)).toEqual([1,2]);
+            expect(arrayLibrary.take(testArray,2)).toEqual([1,2]);
         });
 
         it("returns {1,2,3,4} on ({1,2,3,4}, 4)", function() {
-            expect(arrayLibrary.take(test_array,4)).toEqual([1,2,3,4]);
+            expect(arrayLibrary.take(testArray,4)).toEqual([1,2,3,4]);
         });
 
         it("returns {1,2,3,4} on ({1,2,3,4}, n > length)", function() {
-            expect(arrayLibrary.take(test_array,10)).toEqual(test_array);
+            expect(arrayLibrary.take(testArray,10)).toEqual(testArray);
         });
 
-        it("returns null on n <= 0", function() {
-            expect(arrayLibrary.take([1,2,3,4], -1)).toEqual(null);
+        it("returns elements except last mod(n) on n <= 0", function() {
+            expect(arrayLibrary.take([1,2,3,4], -3)).toEqual([1]);
         });
     });
 
     describe("Method skip tests", function() {
-        it("returns {7,8} on (test_array,6)", function() {
-            expect(arrayLibrary.skip(test_array,6)).toEqual([7,8]);
+        it("returns {7,8} on (testArray,6)", function() {
+            expect(arrayLibrary.skip(testArray,6)).toEqual([7,8]);
         });
 
-        it("returns null on n >= test_array.length", function() {
-            expect(arrayLibrary.skip(test_array,8)).toEqual(null);
+        it("goes round on n >= testArray.length", function() {
+            expect(arrayLibrary.skip(testArray,10)).toEqual([3,4,5,6,7,8]);
         });
 
-        it("returns {2,3,4,5,6,7,8} on (test_array,1)", function() {
-            expect(arrayLibrary.skip(test_array,1)).toEqual([2,3,4,5,6,7,8]);
+        it("returns {2,3,4,5,6,7,8} on (testArray,1)", function() {
+            expect(arrayLibrary.skip(testArray,1)).toEqual([2,3,4,5,6,7,8]);
         });
 
-        it("returns test_array on n <= 0", function() {
-            expect(arrayLibrary.skip(test_array,0)).toEqual(test_array);
+        it("returns testArray on n <= 0", function() {
+            expect(arrayLibrary.skip(testArray,0)).toEqual(testArray);
         });
     });
 
     describe("Method map tests", function() {
         it("reduces each element by 1 if x=> x - 1", function() {
-            expect(arrayLibrary.map(test_array, function(x){ return x - 1;})).
+            expect(arrayLibrary.map(testArray, function(x){ return x - 1;})).
             toEqual([0,1,2,3,4,5,6,7]);
         });
 
         it("squares each element if x => x*x", function() {
-            expect(arrayLibrary.map(test_array, function(x) { return x*x; })).
+            expect(arrayLibrary.map(testArray, function(x) { return x*x; })).
             toEqual([1,4,9,16,25,36,49,64]);
         });
 
         it("doesn't change original array", function() {
-            var map = arrayLibrary.map(test_array, Math.sqrt);
+            var map = arrayLibrary.map(testArray, Math.sqrt);
 
-            expect(test_array).not.toEqual(map);
+            expect(testArray).not.toEqual(map);
         });
     });
 
     describe("Method reduce tests", function() {
         it("returns sum of array elements if x,y => x + y ", function() {
-            expect(arrayLibrary.reduce(test_array, function(a, b) { return a + b; })).toBe(36);
+            expect(arrayLibrary.reduce(testArray, function(a, b) { return a + b; })).toBe(36);
         });
 
         it("returns concat for string array if x,y => x + y", function() {
@@ -74,11 +74,11 @@ describe("ArrayLibrary tests", function() {
         });
 
         it("returns multiplication of array elements if x,y => x * y ", function() {
-            expect(arrayLibrary.reduce(test_array, function(a, b) { return a*b; })).toEqual(24*1680);
+            expect(arrayLibrary.reduce(testArray, function(a, b) { return a * b; })).toEqual(24*1680);
         });
 
         it("returns difference of array elements if x,y => x * y ", function() {
-            expect(arrayLibrary.reduce(test_array, function(a, b) { return a-b; })).toBe(-34);
+            expect(arrayLibrary.reduce(testArray, function(a, b) { return a - b; })).toBe(-34);
         });
 
         it("still works on array with 1 or more elements", function() {
@@ -141,25 +141,33 @@ describe("ArrayLibrary tests", function() {
         });
 
         it("works if callback func arguments.length < 3", function() {
+            var actual = "", list = ["Hi", "Goodbye", "Не скучай"];
 
-        })
+            var func = function(item) {
+                actual += item + "\n";
+            };
+
+            arrayLibrary.foreach(list, func);
+            var expected = "Hi\nGoodbye\nНе скучай\n";
+            expect(actual).toEqual(expected);
+        });
     });
 
     describe("Method filter tests", function() {
         it("doesn't change original array", function() {
-            var moreThanFiveArr = arrayLibrary.filter(test_array, function(number) { return number - 5 > 0; });
+            var moreThanFiveArr = arrayLibrary.filter(testArray, function(number) { return number - 5 > 0; });
 
-            expect(test_array).not.toEqual(moreThanFiveArr);
+            expect(testArray).not.toEqual(moreThanFiveArr);
         });
 
         it("works in normal conditions, when func args.length < 3", function() {
-            var moreThanFiveArr = arrayLibrary.filter(test_array, function(number) { return number - 5 > 0; });
+            var moreThanFiveArr = arrayLibrary.filter(testArray, function(number) { return number - 5 > 0; });
 
             expect(moreThanFiveArr).toEqual([6,7,8]);
         });
 
         it("works in normal conditions, when func args.length = 3", function() {
-            var actualArr = arrayLibrary.filter(test_array, function(number, i, arr) { return (number*i) in arr; });
+            var actualArr = arrayLibrary.filter(testArray, function(number, i, arr) { return (number*i) in arr; });
 
             expect(actualArr).toEqual([1,2,3]);
         });
